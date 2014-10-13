73e7c5ce7aefb3d7207b31fe945051a48acc5236
Chris Guidry <chris@theguidrys.us>
2014-10-12 21:17:20 -0400

    A big initial draft of the multiverse library and reading UIs, with a muted look and minimalist chrome.  Includes Pure CSS and some vanilla CSS and JS.

--- multiverse.py
+++ multiverse.py
@@ -21,32 +21,27 @@ ARCHIVE_TYPES = {
 }
 WEB_IMAGE_TYPES = {'image/png', 'image/jpeg'}
 
+
 def open_archive(full_path):
     mimetype, _ = mimetypes.guess_type(full_path)
-    return ARCHIVE_TYPES[mimetype](full_path)
+    return ARCHIVE_TYPES[mimetype](full_path, mode='r')
 
-def archive_list(archive):
+def archive_files(archive):
     for entry in archive.infolist():
         if getattr(entry, 'isdir', lambda: False)():
             continue
         yield entry.filename
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def archive_pages(archive):
+    return (member for member in sorted(archive_files(archive))
+            if mimetypes.guess_type(member)[0] in WEB_IMAGE_TYPES)
 
-@app.route('/library/<path:path>/pages')
-def pages(path):
-    full_path = os.path.join(app.config['LIBRARY_ROOT'], path)
-    if not os.path.isfile(full_path):
-        abort(404)
-
-    with open_archive(full_path) as archive:
-        uris = ['/library/%s/pages/%s' % (path, member) for member in sorted(archive_list(archive))]
-        return '\n'.join(uris), 200, {'Content-Type': 'text/plain'}
+def archive_title(archive_path):
+    title, _ = os.path.splitext(os.path.basename(archive_path))
+    return title
 
 @app.route('/library/<path:path>/pages/<path:page>')
-def page(path, page):
+def issue_page(path, page):
     full_path = os.path.join(app.config['LIBRARY_ROOT'], path)
     if not os.path.isfile(full_path):
         abort(404)
@@ -54,7 +49,31 @@ def page(path, page):
     with open_archive(full_path) as archive:
         return archive.read(page), 200, {'Content-Type': mimetypes.guess_type(page)[0]}
 
-@app.route('/library/')
+@app.route('/library/<path:path>/cover')
+def cover(path):
+    full_path = os.path.join(app.config['LIBRARY_ROOT'], path)
+    if os.path.isdir(full_path):
+        return series_cover(full_path)
+    elif os.path.isfile(full_path):
+        return issue_cover(full_path)
+    abort(404)
+
+def series_cover(full_path):
+    for path, _, filenames in os.walk(full_path):
+        for filename in filenames:
+            if mimetypes.guess_type(filename)[0] in ARCHIVE_TYPES:
+                try:
+                    return issue_cover(os.path.join(path, filename))
+                except Exception:
+                    continue
+    abort(404)
+
+def issue_cover(full_path):
+    with open_archive(full_path) as archive:
+        cover = next(archive_pages(archive))
+        return archive.read(cover), 200, {'Content-Type': mimetypes.guess_type(cover)[0]}
+
+@app.route('/')
 @app.route('/library/<path:path>')
 def library(path=''):
     full_path = os.path.join(app.config['LIBRARY_ROOT'], path)
@@ -65,36 +84,36 @@ def library(path=''):
     abort(404)
 
 def series(full_path, library_path):
-    series = []
-    issues = []
-    for entry in os.listdir(full_path):
-        if entry.startswith('.'):
+    items = []
+    for filename in sorted(os.listdir(full_path)):
+        if filename.startswith('.'):
             continue
 
-        entry_full_path = os.path.join(full_path, entry)
-        entry_uri = '/library/' + os.path.relpath(entry_full_path, app.config['LIBRARY_ROOT'])
-        if os.path.isdir(entry_full_path):
-            series.append(entry_uri)
-        elif os.path.isfile(entry_full_path):
-            issues.append(entry_uri)
+        entry_full_path = os.path.join(full_path, filename)
+        relative_path = os.path.relpath(entry_full_path, app.config['LIBRARY_ROOT'])
+        items.append({
+            'uri': '/library/' + urllib.parse.quote(relative_path),
+            'title': archive_title(entry_full_path)
+        })
 
     context = {
         'title': library_path,
-        'series': series,
-        'issues': issues
+        'items': items
     }
-    return render_template('series.html', **context)
+    return render_template('library.html', **context)
 
 def issue(full_path, library_path):
     series_path, archive_name = os.path.split(full_path)
     with open_archive(full_path) as archive:
         context = {
-            'title': archive_name,
-            'pages': [urllib.parse.quote('/library/%s/pages/%s' % (library_path, member))
-                      for member in sorted(archive_list(archive))
-                      if mimetypes.guess_type(member)[0] in WEB_IMAGE_TYPES]
+            'title': archive_title(full_path),
+            'pages': [{
+                'uri': urllib.parse.quote('/library/%s/pages/%s' % (library_path, page)),
+                'filename': urllib.parse.quote(page)
+            } for page in archive_pages(archive)]
         }
-    return render_template('comic.html', **context)
+    return render_template('reader.html', **context)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ static/multiverse.js
@@ -0,0 +1,81 @@
+(function(){
+    "use strict";
+
+    var html = document.querySelector('html');
+
+    function common() {
+    }
+
+    function library() {
+    }
+
+    function reader() {
+        var pageList = document.getElementById('pages'),
+            displayTools = document.getElementById('displayTools'),
+            displayClasses = [].map.call(displayTools.querySelectorAll('a'), function(el) { return el.id; });
+
+        function selectDisplay(tool) {
+            [].forEach.call(document.querySelectorAll('#displayTools>a'), function(otherTool) {
+                otherTool.classList.remove('active');
+            });
+            displayClasses.forEach(function(displayClass) {
+                pageList.classList.remove(displayClass);
+                html.classList.remove(tool.id);
+            });
+
+            tool.classList.add('active');
+            html.classList.add(tool.id);
+            pageList.classList.add(tool.id);
+            [].forEach.call(pageList.querySelectorAll('li'), function(page) {
+                page.classList.remove('pure-u-1-1');
+                page.classList.remove('pure-u-1-2');
+                if (tool.id === 'twoPages') {
+                    page.classList.add('pure-u-1-2');
+                } else if (tool.id === 'fitWidth' || tool.id === 'fitHeight') {
+                    page.classList.add('pure-u-1-1');
+                }
+            });
+
+            localStorage.setItem('display', tool.id);
+        }
+
+        displayTools.addEventListener('click', function(event) {
+            var tool = event.target;
+            if (tool.tagName !== 'A') {
+                return;
+            }
+            selectDisplay(tool);
+        });
+
+        document.addEventListener('keyup', function(event) {
+            switch (event.keyCode) {
+                case 49: // 1
+                    selectDisplay(document.getElementById('fitWidth'));
+                    break;
+                case 50: // 2
+                    selectDisplay(document.getElementById('twoPages'));
+                    break;
+                case 51: // 3
+                    selectDisplay(document.getElementById('fitHeight'));
+                    break;
+                case 52: // 4
+                    selectDisplay(document.getElementById('leftToRight'));
+                    break;
+            }
+        });
+
+        if (localStorage.getItem('display')) {
+            var initialTool = document.getElementById(localStorage.getItem('display')) ||
+                              document.getElementById('fitWidth');
+            selectDisplay(initialTool);
+        }
+    }
+
+    common();
+    if (html.classList.contains('library')) {
+        library();
+    }
+    if (html.classList.contains('reader')) {
+        reader();
+    }
+}());
